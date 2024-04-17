357a23942ead808d03797db350b6d5bab5fbd297
Marcelo Amador <marcelo@cklabs.dev>
2024-04-17 09:56:00 -0300

    Basic MAUI app

---
+++ FirebaseMauiApp/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace FirebaseMauiApp
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ FirebaseMauiApp/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace FirebaseMauiApp
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
